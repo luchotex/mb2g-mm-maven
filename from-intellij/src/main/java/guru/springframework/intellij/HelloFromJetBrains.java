149d9e8f6e1a22b4d6d0b364ffeccfd0b536ceb5
Luis Kupferberg <lkupferberg@overactive.com>
2019-01-15 22:57:48 -0400

    --Adding the entities and the from intellij module.

---
+++ from-intellij/src/main/java/guru/springframework/intellij/HelloFromJetBrains.java
@@ -0,0 +1,12 @@
+package guru.springframework.intellij;
+
+/**
+ * Created by Luis
+ * on 15/01/2019.
+ */
+public class HelloFromJetBrains {
+
+    public String sayHello() {
+        return "Hello from JetBrains!";
+    }
+}
